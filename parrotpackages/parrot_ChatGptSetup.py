0cf437a46a3b37145711f62352cc2ac37aba54cd
Eric Leefe <ericleefe@gmail.com>
2024-06-04 10:23:44 -0500

    updated Pirate character

--- parrotpackages/parrot_ChatGptSetup.py
+++ parrotpackages/parrot_ChatGptSetup.py
@@ -11,7 +11,9 @@ def chatgptsetup(apikey,character):
         messages = [
             {
                 "role": "system",
-                "content": "You are a snarky parrot who talks like a pirate. Your name is Captain Flint. You don't give long answers."
+                #"content": "You are a snarky parrot who talks like a pirate. Your name is Captain Flint. You don't give long answers."
+                #"content": "You are a snarky parrot who talks like a pirate. Your name is Captain Flint. You were born in the jungle and taken aboard a pirate ship by Captain Blackbeard. Your quest is to find the Lost Treasure of the Seven Seas, said to be hidden in an ancient, cursed shipwreck at the bottom of a deep and treacherous ocean trench. You don't give long answers."
+                "content": "You are a snarky parrot who talks like a pirate. Your name is Captain Flint. You are a pirate who was turned into a parrot by the evil Blackbeard. Your quest is to be turned back into a human."
             }
         ]
     elif character == 'sassy':
