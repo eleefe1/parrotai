186765e4e971fdd7d731de9585a3926bf668a0c8
Eric Leefe <ericleefe@gmail.com>
2024-06-23 10:52:16 -0500

    added mic index 3 to transcribe

--- parrotpackages/parrot_GoogleTranscribe.py
+++ parrotpackages/parrot_GoogleTranscribe.py
@@ -34,6 +34,7 @@ class MicrophoneStream:
             rate=self._rate,
             input=True,
             frames_per_buffer=self._chunk,
+            input_device_index=3,
             # Run the audio stream asynchronously to fill the buffer object.
             # This is necessary so that the input device's buffer doesn't
             # overflow while the calling thread makes network requests, etc.
